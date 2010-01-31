0b0758a92d8fca5817e20c4a38eb43c7833c595f
patrick <patrick>
2010-01-31 14:27:31 +0000

    updated version number where missing

--- build/nsEnigModule.h
+++ build/nsEnigModule.h
@@ -38,6 +38,6 @@
 #ifndef _nsEnigModule_h_
 #define _nsEnigModule_h_
 
-#define ENIGMIME_VERSION "1.0"
+#define ENIGMIME_VERSION "1.0.1"
 
 #endif
--- ui/content/enigmailCommon.js
+++ ui/content/enigmailCommon.js
@@ -34,8 +34,8 @@ GPL.
 // enigmailCommon.js: shared JS functions for Enigmail
 
 // This Enigmail version and compatible Enigmime version
-var gEnigmailVersion = "1.0";
-var gEnigmimeVersion = "1.0";
+var gEnigmailVersion = "1.0.1";
+var gEnigmimeVersion = "1.0.1";
 
 // Maximum size of message directly processed by Enigmail
 const ENIG_MSG_BUFFER_SIZE = 96000;
