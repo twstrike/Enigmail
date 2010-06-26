76c8b269fc8f9fbd5288cc0aa7674f27bcdadb63
patrick <patrick>
2010-06-26 17:52:32 +0000

    fixed bug 22959

--- package/enigmail.js
+++ package/enigmail.js
@@ -1231,7 +1231,7 @@ function () {
                .getInstallLocation(ENIGMAIL_EXTENSION_ID);
       var extensionLoc = installLoc.getItemFile(ENIGMAIL_EXTENSION_ID, "wrappers");
       extensionLoc.append("gpg-agent-wrapper.sh");
-
+			extensionLoc.permissions=0755;
       agentProcess = Components.classes[NS_PROCESS_UTIL_CONTRACTID].createInstance(Components.interfaces.nsIProcess);
       agentProcess.init(extensionLoc);
       agentProcess.run(true, [ "stop", this.gpgAgentProcess ], 2);
@@ -1758,6 +1758,7 @@ function (domWindow) {
                    .getInstallLocation(ENIGMAIL_EXTENSION_ID);
           var extensionLoc = installLoc.getItemFile(ENIGMAIL_EXTENSION_ID, "wrappers");
           extensionLoc.append("gpg-agent-wrapper.sh");
+          extensionLoc.permissions=0755;
           args.unshift(command.path);
           args.unshift("start");
           command = extensionLoc;
