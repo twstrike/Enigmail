2ed9e6b91f6584e6e77b09e93ffe5068fd9bb2d4
Fan Jiang <fanjiang@thoughtworks.com>
2015-04-30 11:40:40 -0500

    add env for test display

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+display=$(ps aux | grep "[Xvfb] :" | awk '{print $12}')
+
+export DISPLAY=$display
 for f in `find . -type d -name 'tests'`; do 
     pushd . > /dev/null
     cd $f
