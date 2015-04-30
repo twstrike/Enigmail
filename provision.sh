23709fc3cb703bbd71fc1e2cfdd64caa95c8cbe9
Ola Bini <ola.bini@gmail.com>
2015-04-30 16:35:28 -0500

    Do not do an apt-get upgrade

--- provision.sh
+++ provision.sh
@@ -3,7 +3,7 @@ export DEBIAN_FRONTEND=noninteractive
 echo "Provisioning ..."
 sudo apt-get update
 sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" zip thunderbird xvfb
-sudo apt-get upgrade -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+#sudo apt-get upgrade -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
 wget -O /tmp/jsunit-0.1.xpi https://www.enigmail.net/jsunit/jsunit-0.1.xpi
 unzip /tmp/jsunit-0.1.xpi -d /usr/lib/thunderbird-addons/extensions/jsunit@enigmail.net
 echo "/enigmail-src/build/dist" > '/usr/lib/thunderbird-addons/extensions/{847b3a00-7ab1-11d4-8f02-006008948af5}'
