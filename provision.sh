15b55d868f3816441a0a1b4d48078bfbe7ad2f71
Ola Bini <ola.bini@gmail.com>
2015-04-30 16:57:02 -0500

    Brackets and braces and quotes

--- provision.sh
+++ provision.sh
@@ -6,4 +6,4 @@ sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options
 #sudo apt-get upgrade -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
 wget -O /tmp/jsunit-0.1.xpi https://www.enigmail.net/jsunit/jsunit-0.1.xpi
 sudo unzip /tmp/jsunit-0.1.xpi -d /usr/lib/thunderbird-addons/extensions/jsunit@enigmail.net
-sudo /bin/bash -c 'echo "/enigmail-src/build/dist" > \'/usr/lib/thunderbird-addons/extensions/{847b3a00-7ab1-11d4-8f02-006008948af5}\''
+sudo /bin/bash -c 'echo /enigmail-src/build/dist > /usr/lib/thunderbird-addons/extensions/{847b3a00-7ab1-11d4-8f02-006008948af5}'
