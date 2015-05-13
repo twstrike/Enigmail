a4f7fd30a92c698fcb1fb78979ccec49aed2da55
Ola Bini <ola.bini@gmail.com>
2015-05-13 13:36:42 +0100

    Change how we notice output from the unit testing framework

--- util/run-tests.py
+++ util/run-tests.py
@@ -85,7 +85,7 @@ class TestRunner:
         return tsk.wait()
 
     def is_jsunit(self, str):
-        return str.startswith("TestResult: ") or "resource://jsunit/jsunit-main.jsm" in str
+        return str.startswith("TestResult: ") or str.startswith("AssertionError: ")
 
     def extract_number(self, str):
         return int(re.search('\d+', str).group(0))
