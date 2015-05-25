1463d3575b62e6fac77b624b23af2a4aedc0d8e5
Ola Bini <ola.bini@gmail.com>
2015-05-25 13:59:34 -0500

    Update docker helper scripts to be independent of place. Also add some files to gitignore

--- provisioning/docker-init.sh
+++ provisioning/docker-init.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
+CURRENT_FILE=`realpath "$0"`
+PROVISIONING_DIR=`dirname "$CURRENT_FILE"`
+ENIGMAIL_ROOT=`dirname "$PROVISIONING_DIR"`
+
+pushd .
+cd $PROVISIONING_DIR
+
 docker build -t enigmail-unit .
+
+popd
