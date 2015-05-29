628d4139fea9493acb79f2793e73d893e68d2717
Ola Bini <ola.bini@gmail.com>
2015-05-29 12:15:02 -0500

    Remove weird duplicated test helpers

--- package/tests/testHelper.js
+++ package/tests/testHelper.js
@@ -73,18 +73,6 @@ var TestHelper = {
                 TestHelper.allTests[i]();
             }
         }
-    },
-
-    Assert: {
-        assertContains: function(actual, expected, message) {
-            var msg = message || "Searching for <".concat(expected)
-                    .concat("> to be contained within ")
-                    .concat("<").concat(actual).concat(">");
-            this.report(actual.search(expected) == -1, actual, expected, message, "contains");
-        },
-        deepEqual: function(actual, expected, message) {
-            this.report(!(actual === expected), actual, expected, message, "===");
-        }
     }
 };
 
