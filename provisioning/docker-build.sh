5fe7a403e80298090cf62cd0cafa7242f7a80a29
Fan Jiang <fanjiang@thoughtworks.com>
2015-06-01 13:27:07 -0500

    fix docker build script to fit with mac boot2docker usage

--- provisioning/docker-build.sh
+++ provisioning/docker-build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-CURRENT_FILE=`realpath "$0"`
+if command -v realpath>/dev/null 2>&1; then
+    CURRENT_FILE=`realpath "$0"`
+else
+    CURRENT_FILE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/$(basename $0)"
+fi
 PROVISIONING_DIR=`dirname "$CURRENT_FILE"`
 ENIGMAIL_ROOT=`dirname "$PROVISIONING_DIR"`
 
