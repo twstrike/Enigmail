be4db71568efec0fc4db014bfd443b6751e7ae23
Iván Pazmiño <iapazmino@gmail.com>
2015-06-12 14:34:47 -0500

    Removed display remapping.

--- provisioning/provision.sh
+++ provisioning/provision.sh
@@ -10,4 +10,3 @@ rm -rf /usr/lib/thunderbird-addons/extensions/jsunit@enigmail.net
 unzip /tmp/jsunit-0.1.xpi -d /usr/lib/thunderbird-addons/extensions/jsunit@enigmail.net
 rm -rf '/usr/lib/thunderbird-addons/extensions/{847b3a00-7ab1-11d4-8f02-006008948af5}'
 echo "/enigmail-src/build/dist" > '/usr/lib/thunderbird-addons/extensions/{847b3a00-7ab1-11d4-8f02-006008948af5}'
-nohup Xvfb :99 >/dev/null 2>&1 &
